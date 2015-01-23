eedd15c92ae0555b382e4d79f151dce3cbaeaf04
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2015-01-23 17:08:59 -0200

    Check signature.

--- app.rb
+++ app.rb
@@ -22,14 +22,43 @@ request.body
 request.env
 -----------
 #{request.env.pretty_inspect}
+
   HTML
 end
 
-post '/echo' do
-  request.body.read.to_json.pretty_inspect
+post '/check' do
+  check_request(request)
 end
 
 post '/webhook' do
   pp "Receiving Webhook #{params[:webhookId]}"
+end
+
+
+###
+### Ref: https://developer.github.com/webhooks/securing/
+###
+def check_request(request)
+  request.body.rewind
+  payload = request.body.read
+  verify_signature(payload)
+  payload
+end
+
+def verify_signature(payload)
+
+  if not request.env.has_key?('HTTP_X_HUB_SIGNATURE')
+    logger.warn("X_HUB_SIGNATURE is not defined. Skipping...")
+    return
+  end
+
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest( OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET_TOKEN'], payload )
+
+  if Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    logger.info("Signature OK: #{signature}")
+  else
+    logger.error("Signature error: #{signature}. Expected #{request.env['HTTP_X_HUB_SIGNATURE']}")
+    return halt 500, "Signatures didn't match!"
+  end
 end
 
