d457018a0ffb3d784371bb5ea130d4c6514fffd4
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2015-01-28 20:16:55 -0200

    Github issue number as metadata inside 'notes'.

--- lib/asana.rb
+++ lib/asana.rb
@@ -24,9 +24,9 @@ post '/asana/issue/:workspace/:project' do
 
 
   # parent task
-  data = %Q{-d "workspace=#{w}" -d "projects[0]=#{p}" -d "name=#{github['issue']['title']}" -d "notes=#{github['issue']['body']}" }
   ## POST /tasks
   ##
+  data = %Q{-d "workspace=#{w}" -d "projects[0]=#{p}" -d "name=#{github['issue']['title']}" -d "notes=github_issue_number: #{github['issue']['number']}" }
   cmd  = "curl -s #{asana_api}/tasks #{data}"
   logger.info("asana task cmd: #{cmd}")
 
