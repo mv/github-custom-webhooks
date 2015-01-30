0cd5900a753c0325d3ea4a5b4f454c38e4012042
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2015-01-30 15:25:01 -0200

    Using Asana as a class.

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require 'pp'
 require_relative 'lib/asana'
 require_relative 'lib/github'
 
+set :logging, true
+
 get '/' do
   "Test."
 end
@@ -14,22 +16,43 @@ get '/webhook' do
 end
 
 post '/github/check' do
-  g = GitHub::Check.new(request).payload
+  github = GitHub::Check.new(request)
+  github.payload
 end
 
-post '/request' do
-  response = <<-HTML
-<html><pre>
-
-request.body
-------------
-#{JSON.parse(request.body.read).pretty_inspect}
-</pre></html>
-
-request.env
------------
-#{request.env.pretty_inspect}
-
-  HTML
+get '/asana' do
+  "This is Asana."
 end
+
+post '/asana/issue/:workspace/:project' do
+
+  # github: check current 'request' that was triggered by Github project
+  github = GitHub::Check.new(request).payload
+
+  # asana: if payload is a 'issue', create task/subtasks
+  asana_task = Asana::Task.new(params[:workspace], params[:project])
+
+  new_issue = {
+    'name'   => github['issue']['title'],
+    'number' => github['issue']['number']
+  }
+
+  parent_id  = asana_task.create(new_issue)
+
+  # optional(?)
+  asana_task.create_subtasks(parent_id)
+
+  # Polite result
+  "New task: #{parent_id}"
+
+# error do
+#   if asana_task.check_key
+#     'Asana: Sorry there was a nasty error - ' + env['sinatra.error'].name
+#   else
+#     'Asana UNAUTHORIZED: API Key is not defined.'
+#   end
+# end # error
+
+end
+
 
