daffebfaafc08ff8133c579a3b67b3e0c0069214
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2015-01-30 15:24:42 -0200

    spec: renaming tests.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -2,15 +2,15 @@
 
 require File.expand_path '../spec_helper.rb', __FILE__
 
-describe "Sinatra: Webhooks" do
+describe "Webhooks - Home" do
 
-  it "should allow accessing the home page" do
+  it "should GET '/'" do
     get '/'
     expect(last_response).to be_ok
     expect(last_response.body).to eq('Test.')
   end
 
-  it "/webhook entrypoint" do
+  it "should GET '/webhook'" do
     get '/webhook'
     expect(last_response).to be_ok
     expect(last_response.body).to eq('Hello Webhook.')
